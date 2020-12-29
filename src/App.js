33a2d6c0f5c9a26947edf58125c24c789bc23c0e
Luis <luisprooc@gmail.com>
2020-12-29 17:45:08 -0400

    Eventos de los inputs

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
 import React,{useState} from 'react';
 
 
-const MouseEvents = () => {
-    let [x, setX] = useState(0);
-    let [y, setY] = useState(0);
 
-    const styles = {
-        background: "silver",
-        height: "40em",
-    
-    }
-
-    const coord = {
-        background: "lightGreen",
-        height: "40em",
-    }
-
-    const calcCoord = (e) => {
-        setX(e.clientX);
-        setY(e.clientY);
-    };
-    
-
-    return(
-        <>  
-            <h1>Down</h1>
-            <div style={styles} onMouseDown={ () => alert("H")}> </div>
-            <h1>UP</h1>
-            <div style={styles} onMouseUp={ () => alert("Up")}> </div>
-
-            <h1>Saber mis cordenadas: </h1>
-            
-            <div style={coord} onMouseMove={ e => calcCoord(e)}> 
-                <h2>X: {x?x :null} </h2>
-                <h2>Y: {y?y :null} </h2>
-            </div>
-        </>
-    );
-};
 
 
 const App = () => {
+    let [text,setText] = useState('');
+    let [event,setEvent] = useState('');
 
+    const manejador = (e) => {
+        if(e.type === "paste"){
+            alert("no");
+            return;
+        }
+        setText(e.target.value);
+    };
+    
     return(
         <>
-            <MouseEvents />
+            <input type="text" onChange={(e)=>{manejador(e)}} onPaste={(e)=>{manejador(e)}} />
+            <h3>{text}</h3>
+            <h2>{event}</h2>
         </>
     );
 };
