d1df44eca4d61dbaa15d8302b139772d5bd7bbb5
Luis <luisprooc@gmail.com>
2021-01-07 16:15:42 -0400

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-import React,{useState} from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Portal = ({children,visible}) => {
-    const styles ={
-        width:'100%',
-        background:'red',
-        height: '100vh',
-        color: 'white'
-    }
-
-    
-    return visible?(ReactDOM.createPortal((
-        <div style={styles}>
-            <h1>{children}</h1>
-        </div>
-    ), 
-    document.getElementById("modal")))
-    :null;
+const Profile = ({name,saldo}) => {
+    return(
+        <>
+            <h2>{name}</h2>
+            <h4>{saldo + 100}</h4>
+        </>
+    )
 };
 
+Profile.propTypes = {
+    name: PropTypes.string,
+    saldo: PropTypes.number
+}
 
 const App = () => {
-    let [visible,setVisible] = useState(false);
-
-    const handler = () =>{
-        if(visible) return setVisible(false);
-
-        return setVisible(true);
-    }
 
     return(
         <>  
-            <button onClick={()=> handler()}>{!visible?"Mostrar":"Ocultar"}</button>
-            <Portal visible={visible}>
-                <em>Hola</em>
-            </Portal>
+            <Profile name={"Pedro"} saldo={"true"}/>
         </>
     );
 };
