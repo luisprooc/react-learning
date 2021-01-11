51a4e9b88bd0645f95089e474348feb2eee30002
Luis <luisprooc@gmail.com>
2021-01-11 15:00:02 -0400

    Refs en librerias de terceros

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React,{createRef} from 'react';
+import React,{createRef,useEffect} from 'react';
 import Chart from "chart.js";
 
 
-class Graficas extends React.Component{
-    grafica = createRef();
 
-    componentDidMount(){
-        const ctx = this.grafica.current.getContext('2d');
+const Graficas = () => {
+    const grafica = createRef();
+
+    const grafic = () => {
+        const ctx = grafica.current.getContext("2d");
+
         new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['Red', 'Blue', 'Gray', 'Green', 'Purple', 'Orange'],
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                 datasets: [{
                     label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    data: [12, 13, 3, 5, 2, 3],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
-                        'Gray',
+                        'rgba(255, 206, 86, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
                         'rgba(153, 102, 255, 0.2)',
                         'rgba(255, 159, 64, 0.2)'
@@ -25,12 +27,12 @@ class Graficas extends React.Component{
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
-                        'Gray',
+                        'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
                         'rgba(153, 102, 255, 1)',
                         'rgba(255, 159, 64, 1)'
                     ],
-                    borderWidth: 2
+                    borderWidth: 1
                 }]
             },
             options: {
@@ -43,16 +45,20 @@ class Graficas extends React.Component{
                 }
             }
         });
-    }
+    };
+    
 
-    render(){
-        return(
-            <>
-                <canvas width="200" height="200" ref={this.grafica}></canvas>
-            </>
-        )
-    }
-} 
+    useEffect(()=>{
+        grafic()
+    },[grafic]);
+
+    return(
+        <>
+            <canvas width="700" height="700" ref={grafica}></canvas>
+        </>
+    )
+};
+
 
 
 const App = () => {
