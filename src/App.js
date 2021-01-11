a66104025cba9e586b5ea2bd838bf15cd05a1304
Luis <luisprooc@gmail.com>
2021-01-11 16:57:24 -0400

    Inputs no controlados con formularios

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import React,{createRef,useEffect,forwardRef} from 'react';
+import React,{createRef,useEffect} from 'react';
 
-
-const FancyInput = forwardRef((props,ref) => {
+const Bird = () => {
     return(
         <>
-            <input type="text" ref={ref}/>
+            <span role="img" aria-label="bird">
+                🦅
+            </span>
         </>
     );
-});
+};
 
+const Inputs = (props) => {
+
+    const handler = (e) => {
+        e.preventDefault();
+        const nombre = e.target[0].value;
+        const password = e.target[1].value;
+        props.onSend({nombre,password});
+    };
+    
+
+    return(
+        <form onSubmit={(e)=>handler(e)}>
+            <input type="text"  placeholder="Name"/>
+            <input type="password"  placeholder="password"/>
+            <button>SEND</button>
+        </form>
+    );
+};
 
 const App = () => {
 
-    const entrada = createRef();
-    
-    useEffect(()=>{
-        console.log(entrada)
-    });
+    const onSent = (data) =>{
+        console.log(data);
+    }
 
     return(
         <>  
-            <FancyInput
-                ref={entrada}
+            <h1>Inputs no controlados <Bird /></h1>
+            <Inputs 
+                onSend={onSent}
             />
         </>
     );
