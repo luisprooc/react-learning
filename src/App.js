f0b02d8e0127648a8379caf45f30c09ac129c772
Luis <luisprooc@gmail.com>
2021-01-11 15:16:00 -0400

    Refs de padre a hijos

--- src/App.js
+++ src/App.js
@@ -1,72 +1,28 @@
-import React,{createRef,useEffect} from 'react';
-import Chart from "chart.js";
+import React,{createRef,useEffect,forwardRef} from 'react';
 
 
-
-const Graficas = () => {
-    const grafica = createRef();
-
-    const grafic = () => {
-        const ctx = grafica.current.getContext("2d");
-
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 13, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-    };
-    
-
-    useEffect(()=>{
-        grafic()
-    },[grafic]);
-
+const FancyInput = forwardRef((props,ref) => {
     return(
         <>
-            <canvas width="700" height="700" ref={grafica}></canvas>
+            <input type="text" ref={ref}/>
         </>
-    )
-};
-
+    );
+});
 
 
 const App = () => {
+
+    const entrada = createRef();
     
+    useEffect(()=>{
+        console.log(entrada)
+    });
+
     return(
         <>  
-            <h1>Graficas 🐗</h1>
-            <Graficas/>
+            <FancyInput
+                ref={entrada}
+            />
         </>
     );
 };
