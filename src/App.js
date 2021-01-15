5d60c79b8abea4ab5925f14c2a5d448b7eef3bcc
Luis <luisprooc@gmail.com>
2021-01-15 09:22:25 -0400

    Select multiple

--- src/App.js
+++ src/App.js
@@ -14,18 +14,27 @@ const Bird = () => {
 
 const App = () => {
 
-    const [tech, setTech] = useState("React");
+    const [techs, setTechs] = useState([]);
+
+    const handler = (e) => {
+        const techs = Array.from(e.target.selectedOptions).map(option => option.value);
+        setTechs(techs);
+    };
+    
 
     return(
         <>  
             <h1>Etiqueta Select <Bird /></h1>
             <form>
-                <select value={tech} onChange={e => setTech(e.target.value)}>
+                <select  value={techs} onChange={handler} multiple>
                     <option value="Angular">Angular</option>
                     <option value="React">React</option>
                     <option value="Vue">Vue</option>
                 </select>
             </form>
+            <ul>
+                {techs.map(value => <li>{value}</li>)}
+            </ul>
         </>
     );
 };
