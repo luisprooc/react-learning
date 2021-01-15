0d42d30cb87b0a831de4c7581486334a35189613
Luis <luisprooc@gmail.com>
2021-01-15 09:10:49 -0400

    Select en formularios

--- src/App.js
+++ src/App.js
@@ -10,51 +10,22 @@ const Bird = () => {
     );
 };
 
-const Inputs = ({placeholder,name,onChange}) => {
 
-
-    const handler = (e) => {
-        const text = e.target.value; 
-
-        onChange(name,text);
-    };
-    
-
-    return(
-        <>
-            <label htmlFor={name}>{name}: </label>
-            <input type="text"  placeholder={placeholder}  name={name} id={name}  onChange={handler}/>
-        </>
-    );
-};
 
 const App = () => {
 
-    const [name,saveName] = useState("");
-    const [email,saveEmail] = useState("");
-
-    const update = (name,data) => {
-        name === "name"? saveName(data):saveEmail(data);
-    };
-    
-
+    const [tech, setTech] = useState("React");
 
     return(
         <>  
-            <h1>Inputs controlados <Bird /></h1>
-            <Inputs 
-                placeholder={"Name"}
-                name={"name"}
-                onChange={update}
-            />
-            <Inputs 
-                placeholder={"Email"}
-                name={"email"}
-                onChange={update}
-            />
-
-            <h3>Name: {name}</h3>
-            <h3>Email: {email}</h3>
+            <h1>Etiqueta Select <Bird /></h1>
+            <form>
+                <select value={tech} onChange={e => setTech(e.target.value)}>
+                    <option value="Angular">Angular</option>
+                    <option value="React">React</option>
+                    <option value="Vue">Vue</option>
+                </select>
+            </form>
         </>
     );
 };
