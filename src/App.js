f3ab80ed980997780118fcebea0ec2b6193d8bbf
Luis <luisprooc@gmail.com>
2021-01-18 09:22:30 -0400

    Solicitud HTTP

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 
+const API = ({catchPosts}) => {
+    const [charge,saveCharge] = useState(true);
+
+    useEffect(()=>{
+        fetch("http://jsonplaceholder.typicode.com/posts")
+            .then(res => res.json())
+            .then(res => sendPosts(res))
+            .catch(error => console.log(error))
+    },[]);
+
+    const sendPosts = (posts) => {
+        posts = posts.splice(0,10);
+        saveCharge(false);
+        catchPosts(posts);
+    };
+    
+    if(charge){
+        return <h1>Chargin.......</h1>
+    }
+
+    return(
+        <>
+            <h1 style={{color:"blue"}}>
+                POSTS: 
+            </h1>
+        </>
+    );
+};
 
 
 const App = () => { 
+    const [posts,savePosts] = useState([]);
 
-    const [bug,setBug] = useState(false);
-
-    const error = () => {
-        setBug(true);
-        
-    };
-
-    if(bug){
-        throw new Error("He fallado 😥😥");
+    const catchPosts = (res) =>{
+        savePosts(res);
     }
-    
 
     return(
         <>  
-            <button 
-            onClick={
-                ()=> error()
-            }>Bugg</button>
+            <API
+                catchPosts={catchPosts}
+            />
+            {posts.map(post=>{
+
+                return(
+                    <div key={post.id}>
+                        <h2>Title: {post.title}</h2> 
+                        <h4>UserID: {post.id}</h4>
+                        <p>Body: {post.body}</p>
+                        <hr/>
+                    </div>
+                )
+            })}
         </>
     );
 };
