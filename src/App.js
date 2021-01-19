ea90892cea2eacd14c7d9d617f61a0c9bebfe6a3
Luis <luisprooc@gmail.com>
2021-01-19 08:59:51 -0400

    Axios y async await

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React,{useState} from 'react';
+import axios from "axios";
 
 const API = () => {
     const [movie,saveMovie] = useState({});
-
-
-    const searchMovie = (e) => {
+    const [show,setShow] = useState(false);
+    
+    const searchMovie = async(e) => {
         e.preventDefault();
         const query = e.target[0].value;
         const url = `https://www.omdbapi.com/?t=${query}&apikey=a9a1b0b6`
 
-        fetch(url)
-            .then(res => res.json())
-            .then(res => saveMovie(res))
+        const res = await axios.get(url);
+
+        // Con fetch
+
+        //const res = await fetch(url);
+        //const data = res.json();
+        
+        saveMovie(res.data);
+        setShow(true);
     };
     
 
@@ -26,12 +33,14 @@ const API = () => {
                 <button>Send</button>
             </form>
 
+            {show &&(
             <div>
                 <h1>Name: {movie.Title}</h1>
                 <h4>Resume: {movie.Plot}</h4>
                 <p>Actors: {movie.Actors}</p>
                 <img src={movie.Poster}></img>
             </div>
+            )}
         </>
     );
 };
