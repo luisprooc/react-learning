1e83f0d3d79ea7e2774f64e4fbfd2aa816406d5a
Luis <luisprooc@gmail.com>
2021-02-15 17:30:43 -0400

    Desmontando useEffet

--- src/App.js
+++ src/App.js
@@ -1,50 +1,35 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 
 
 
 const App = () => { 
 
-    const [data] = useState({
-        id:10,
-        question:"COMO ME LLAMO?",
-        answers:[
-            {   
-                optId: 100,
-                opt: "Carlos"
-            },
-            {   
-                optId: 10,
-                opt: "Luis"
-            },
-            {   
-                optId: 20,
-                opt: "Juan"
-            }
-        ],
-        correct: "Luis"
-    });
+    const [posX, setPosX] = useState(0);
+    const [posY, setPosY] = useState(0);
 
-    const [res,saveRes] = useState("");
+    
+    //DidMound
+    //DiuUpdate
+    useEffect(()=>{
 
-    const handler = (e) => {
-        saveRes(e.target.firstChild);
-    }
+        window.addEventListener("mousemove",handleMove);
+        
+        // DidwillUnmount
+        return () => {
+            window.removeEventListener("mousemove",handleMove);
+        }
+    },[]);
 
-    const send = () =>{
-        console.log(data.correct,res)
-        data.correct === res?alert("SII"):alert("NELL PRRo");
+    const handleMove = (e) => {
+        setPosX(e.clientX);
+        setPosY(e.clientY);
     }
 
     return(
         <>  
-            <h1>TEST</h1>
-            {data && (
-                <div>
-                    <h2>{data.question}</h2>
-                    {data.answers.sort((a,b) => a.optId - b.optId).map(item => <div onClick={(e)=> handler(e)} key={item.optId} vocab={"HEY"}>{item.opt}</div>)}
-                </div>
-            )}
-            <button onClick={()=> send()}>Evaluar</button>
+            <h1>useEffect</h1>
+            <h4>X: {posX}</h4>
+            <h4>Y: {posY}</h4>
         </>
     );
 };
