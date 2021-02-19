a4dee67e69eeffcd9c0957df234f574f60cb93d8
Luis <luisprooc@gmail.com>
2021-02-19 16:53:50 -0400

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { useDebounce } from 'use-debounce';
+import React, { useReducer } from 'react';
+
+const reducer = ( state,action ) => {
+
+    switch(action.type){
+        case "INCREMENT":
+            return { ...state, clicks: state.clicks + 1 };
+
+        case "DECREMENT":
+            return {...state, clicks: state.clicks - 1 };
+
+        case "CHANGETEXT":
+            return {...state, title: action.value };
+
+        default:
+            return state
+    }
+}
 
 const App = () => { 
 
-    const [name,setName] = useState("");
-    const [search] = useDebounce(name,600);
-    const [post,setPost] = useState({});
+    const [state, dispatch] = useReducer(reducer,{
+        clicks: 0,
+        title: "Hola"
+    });
+    
 
-    const handlerInput = (e) => setName(e.target.value);
+    const increment = () => dispatch({type: "INCREMENT"});
 
-    useEffect(()=>{
-        //debounce
- 
-        fetch(`https://jsonplaceholder.typicode.com/todos/${name}`)
-                .then(response => response.json())
-                .then(post => setPost(post));
-    },[search])
+    const decrement = () => dispatch({type: "DECREMENT"});
+
+    const handlerText = (e) => dispatch({ type: "CHANGETEXT", value:e.target.value });
+
 
     return(
         <>  
-            <h1>EXAMPLE WITH HOOKS</h1>
-            <input 
-                type="text"
-                placeholder="Ingresa un numero"
-                onChange={handlerInput}
-            />
-            {post.id && (
-                <div>
-                    <h1>{post.title}</h1>
-                    <p>completed: {post.completed?"TRUE":"FALSE"}</p>
-                </div>
-            )}
+            <h1>Hook useReducer</h1>
+            <p>{state.title} -- {state.clicks}</p>
+            <input type="text" onChange={handlerText}/>
+            <button onClick={increment}>Add</button>
+            <button onClick={decrement}>Less</button>
         </>
     );
 };
