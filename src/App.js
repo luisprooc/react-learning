5128014b6b14141ec169e220c9dc68bacad8c0de
Luis <luisprooc@gmail.com>
2021-03-23 09:55:09 -0400

    React transition group

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import Slides from './components/Slides';
+import React,{useState} from 'react';
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 const App = () => { 
 
+    const [count, setCount] = useState(0);
+
+    const add = () => setCount(count + 1);
+
     return(
         <>
-            <Slides
-            />
+            <button onClick={add}>
+                +
+            </button>
+            <TransitionGroup>
+                <CSSTransition
+                    timeout={100}
+                    classNames="fade"
+                    key={count}
+                >
+                    <div>
+                        {count}
+                    </div>
+                </CSSTransition>
+
+            </TransitionGroup>
         </>
     );
 };
