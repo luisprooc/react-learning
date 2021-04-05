954199d42f0190b489dc5187ca437ad992c712f0
Luis <luisprooc@gmail.com>
2021-04-05 09:12:33 -0400

    Middlewares en redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,35 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
 
 
-export default createStore(reducers);
+// Midleware
+
+const logger = (store) => (next) => (action) => {
+    console.log("Ha ocurrido una accion", action);
+
+    setTimeout(() => {
+        next(action);
+    },1000);
+
+    if(action.type !== "ADD_TODO"){
+        
+        setTimeout(() => {
+    
+            store.dispatch({
+                type: "ADD_TODO",
+                payload: {
+                    text: "Todo creado en Midleware",
+                    checked: false,
+                    id: "DDRLLLLD12"
+                }
+            });
+    
+        },2500);
+    }
+
+};
+
+const store = createStore(reducers, applyMiddleware(logger));
+
+
+export default store;
