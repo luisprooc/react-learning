b9bb252a18d6aa8b8900b8ec3e5637aad87e90a8
Luis Rosario <luisprooc@gmail.com>
2021-04-06 17:28:44 -0300

    Pruebas a numeros

---
+++ src/test/numbers.test.js
@@ -0,0 +1,19 @@
+import { sum, rest , mult,getRandomNum } from "../utils/numbers";
+
+describe("Prueba funciones matematicas", () => {
+    test("Prueba la suma", () => {
+        expect( sum(5,5) ).toBe(10);
+    });
+
+    test("Prueba la resta", () => {
+        expect( rest(10,5) ).toBe(5);
+    });
+
+    test("Prueba la multiplicacion", () => {
+        expect( mult(2,2) ).toBe(4);        
+    });
+
+    test("Prueba a la funcion random", () => {
+        expect( getRandomNum(5,10) ).toBeGreaterThanOrEqual(5);        
+    });
+});
---
+++ src/utils/numbers.js
@@ -0,0 +1,7 @@
+export const sum = (a,b) => a + b;
+
+export const rest = (a,b) => a - b;
+
+export const mult = (a,b) => a * b;
+
+export const getRandomNum = (min,max) => Math.ceil(Math.random() * ( max - min + 1) + min);
