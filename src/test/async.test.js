1f4ffda999254a658d68a41a776e86c83d7fbda5
Luis Rosario <luisprooc@gmail.com>
2021-04-07 17:56:16 -0300

    test a promises con async

--- src/test/async.test.js
+++ src/test/async.test.js
@@ -2,11 +2,19 @@ import {  getData,getDataPromise } from "../utils/async";
 
 describe("Testeando operaciones asincronas", () => {
 
-    test("Haciendo test a promesa", () => {
-        return expect(getDataPromise()).resolves.toEqual([1,2,3]);
+    test("Haciendo test a promesa con async", async() => {
+        const arr = await getDataPromise();
+        expect(arr).toEqual([1,2,3]);
+
     });
 
-    test("Haciendo test a promesa reject", () => {
-        return expect(getDataPromise(true)).rejects.toBe("REJECTED");
+    test("Haciendo test a promesa reject", async() => {
+        try{
+            const res = await getDataPromise(true);
+            expect(res).toBe("REJECTED");
+        }
+        catch(error){
+            expect(error).toBe("REJECTED")
+        }
     });
 });
